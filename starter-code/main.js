8075a63ce794f2b2358fcfa49c3228628dd7f8d1
Sabrina Ooi <heysabs@gmail.com>
2016-06-23 15:28:40 +0800

    Added conditionals

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,16 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
+
+if (cardOne === cardThree) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
