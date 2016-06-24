ad3f5d46e00f47e4bcedde01deaf4d4afc0ebf96
Sabrina Ooi <heysabs@gmail.com>
2016-06-24 17:14:02 +0800

    Added functions

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,18 +1,21 @@
 var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
+/* if (cardOne === cardTwo) {
 	alert("You found a match!");
 }
 else {
 	alert("Sorry, try again.");
-}
+} */
 
-if (cardOne === cardThree) {
-	alert("You found a match!");
-}
-else {
-	alert("Sorry, try again.");
+var board = document.getElementById('game-board');
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+	}
 }
